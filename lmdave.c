4abd76139ad340c544960bdb957d3b3f19895610
Diego Fernandes <diegogkx@gmail.com>
2022-08-28 19:12:05 -0400

    movement and sprite masks

--- lmdave.c
+++ lmdave.c
@@ -1,8 +1,6 @@
 /*
 Dangerous Dave Remake
 Original code by MaiZure 2017
-video stopped at 5:28
-https://youtu.be/Y3lvCywVG94?list=PLSkJey49cOgTSj465v2KbLZ7LMn10bCF9&t=328
 */
 
 #include <stdio.h>
@@ -11,6 +9,8 @@ https://youtu.be/Y3lvCywVG94?list=PLSkJey49cOgTSj465v2KbLZ7LMn10bCF9&t=328
 #include <string.h>
 #include <SDL.h>
 #include "lmdave.h"
+
+const uint8_t TILE_SIZE = 16;
 
 int main(int argc, char* argv[])
 {
@@ -37,9 +37,6 @@ int main(int argc, char* argv[])
   assets = malloc(sizeof(struct game_assets));
   init_assets(assets, renderer);
 
-  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-  SDL_RenderClear(renderer);
-
   while(!game->quit){
     timer_begin = SDL_GetTicks();
 
@@ -49,11 +46,12 @@ int main(int argc, char* argv[])
 
     timer_end = SDL_GetTicks();
 
-    delay = 33 - (timer_end - timer_begin);
+    delay = 16 - (timer_end - timer_begin);
     delay = delay > 33 ? 0 : delay;
-    SDL_Delay(10);
+    SDL_Delay(delay);
   }
 
+  SDL_Quit();
   free(game);
   free(assets);
   
@@ -71,6 +69,22 @@ void init_game(struct game_state *game) {
   game->view_x = 0;
   game->view_y = 0;
   game->scroll_x = 0;
+  game->dave_x = 2;
+  game->dave_y = 8;
+  game->dave_px = game->dave_x * TILE_SIZE;
+  game->dave_py = game->dave_y * TILE_SIZE;
+  game->jump_timer = 0;
+  game->on_ground = 1;
+  game->try_right = 0;
+  game->try_left = 0;
+  game->try_jump = 0;
+  game->dave_right = 0;
+  game->dave_left = 0;
+  game->dave_jump = 0;
+  game->time_scale = 1;
+  game->check_pickup_x = 0;
+  game->check_pickup_y = 0;
+
 
   for (j=0;j<10;j++){
     fname[0]='\0';
@@ -94,9 +108,16 @@ void init_game(struct game_state *game) {
   }
 }
 void init_assets(struct game_assets *assets, SDL_Renderer *renderer){
-  int i;
+  int i, j;
   char fname[13];
   char file_num[4];
+  char mname[13];
+  char mask_num[4];
+  SDL_Surface *surface;
+  SDL_Surface *mask;
+  uint8_t mask_offset;
+  uint8_t *surf_p;
+  uint8_t *mask_p;
 
 
   for(i=0;i<158;i++){
@@ -106,28 +127,150 @@ void init_assets(struct game_assets *assets, SDL_Renderer *renderer){
     strcat(fname, file_num);
     strcat(fname, ".bmp");
 
-    assets->graphics_tiles[i] = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP(fname));
+    if((i>=53 && i<= 59) || i == 67 || i == 68 || (i>=71 && i<=73) || (i>=77 && i<=82))
+    {
+      if(i>=53 && i<=59)
+        mask_offset = 7;
+      if(i>=67 && i<=68)
+        mask_offset = 2;
+      if(i>=71 && i<=73)
+        mask_offset = 3;
+      if(i>=77 && i<=82)
+        mask_offset = 6;
+
+      mname[0]='\0';
+      strcat(mname,"tile");
+      sprintf(&mask_num[0], "%u", i + mask_offset);
+      strcat(mname, mask_num);
+      strcat(mname, ".bmp");
+
+      surface = SDL_LoadBMP(fname);
+      mask = SDL_LoadBMP(mname);
+
+      surf_p = (uint8_t*)surface->pixels;
+      mask_p = (uint8_t*)mask->pixels;
+
+      for (j=0; j< (mask->pitch*mask->h); j++) 
+        surf_p[j] = mask_p[j] ? 0xFF : surf_p[j];
+
+      SDL_SetColorKey(surface, 1, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+      assets->graphics_tiles[i] = SDL_CreateTextureFromSurface(renderer, surface);
+      SDL_FreeSurface(surface);
+      SDL_FreeSurface(mask);
+    } 
+    else 
+      assets->graphics_tiles[i] = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP(fname));
+
   }
 }
+
 void check_input(struct game_state *game){
   SDL_Event event;
   SDL_PollEvent(&event);
+  const uint8_t *keystate = SDL_GetKeyboardState(NULL);
+  if(keystate[SDL_SCANCODE_RIGHT])
+    game->try_right = 1;
+  if(keystate[SDL_SCANCODE_LEFT])
+    game->try_left = 1;
+  if(keystate[SDL_SCANCODE_UP])
+    game->try_jump = 1;
 
   if(event.type == SDL_QUIT)
     game->quit = 1;
+}
 
-  if(event.type == SDL_KEYDOWN){
-    if(event.key.keysym.sym == SDLK_RIGHT)
-      game->scroll_x = 15;
-    if(event.key.keysym.sym == SDLK_LEFT)
-      game->scroll_x = -15;
-    if(event.key.keysym.sym == SDLK_UP)
-      game->current_level++;
-    if(event.key.keysym.sym == SDLK_DOWN)
-      game->current_level--;
+void update_game(struct game_state *game){
+  check_collision(game);
+  pickup_item(game, game->check_pickup_x, game->check_pickup_y);
+  verify_input(game);
+  move_dave(game);
+  scroll_screen(game);
+  apply_gravity(game);
+  clear_input(game);
+}
+
+void render(struct game_state *game, SDL_Renderer *renderer, struct game_assets *assets){
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+  SDL_RenderClear(renderer);
+
+  draw_world(game, assets, renderer);
+  draw_dave(game, assets, renderer);
+
+  SDL_RenderPresent(renderer);
+}
+
+void check_collision(struct game_state *game){
+  game->collision_point[0] = is_clear(game, game->dave_px+4, game->dave_py-1);
+  game->collision_point[1] = is_clear(game, game->dave_px+10, game->dave_py-1);
+  game->collision_point[2] = is_clear(game, game->dave_px+11, game->dave_py+4);
+  game->collision_point[3] = is_clear(game, game->dave_px+11, game->dave_py+12);
+  game->collision_point[4] = is_clear(game, game->dave_px+10, game->dave_py+16);
+  game->collision_point[5] = is_clear(game, game->dave_px+4, game->dave_py+16);
+  game->collision_point[6] = is_clear(game, game->dave_px+3, game->dave_py+12);
+  game->collision_point[7] = is_clear(game, game->dave_px+3, game->dave_py+4);
+  game->on_ground = (!game->collision_point[4] && !game->collision_point[5]);
+}
+
+void verify_input(struct game_state *game){
+  if(game->try_right && game->collision_point[2] && game->collision_point[3]) 
+    game->dave_right = 1;
+  if(game->try_left && game->collision_point[6] && game->collision_point[7]) 
+    game->dave_left = 1;
+  if(game->try_jump && game->on_ground && !game->dave_jump && game->collision_point[0] && game->collision_point[1]) 
+    game->dave_jump = 1;
+}
+
+void move_dave(struct game_state *game) {
+  if(game->dave_right){
+    game->dave_px += 2 * game->time_scale;
+    game->dave_right = 0;
+  }
+  if(game->dave_left){
+    game->dave_px -= 2 * game->time_scale;
+    game->dave_left = 0;
+  }
+  if(game->dave_jump){
+    if(!game->jump_timer)
+      game->jump_timer = 20;
+
+    if(game->collision_point[0] && game->collision_point[1]){
+      if (game->jump_timer > 5) 
+        game->dave_py -= 2 * game->time_scale;
+      else 
+        game->dave_py -= 1 * game->time_scale;
+      
+    }
+    else 
+      game->jump_timer = 0;
+
+    if(!game->jump_timer)
+      game->dave_jump = 0;
   }
 }
-void update_game(struct game_state *game){
+
+void apply_gravity(struct game_state *game){
+  if(!game->dave_jump && !game->on_ground){
+    if(is_clear(game, game->dave_px+4, game->dave_py+17))
+      game->dave_py += 2 * game->time_scale;
+    else {
+      uint8_t not_align;
+      not_align = game->dave_py % TILE_SIZE;
+      if(not_align){
+        game->dave_py = not_align < 8? 
+          game->dave_py - not_align :
+          game->dave_py + TILE_SIZE - not_align;
+      }
+    }
+  }
+}
+
+void clear_input(struct game_state *game){
+  game->try_jump=0;
+  game->try_left=0;
+  game->try_right=0;
+}
+
+void scroll_screen(struct game_state *game){
   if(game->current_level == 0xFF) {
     game->current_level = 0;
   }
@@ -152,23 +295,94 @@ void update_game(struct game_state *game){
   }
 }
 
-void render(struct game_state *game, SDL_Renderer *renderer, struct game_assets *assets){
+void pickup_item(struct game_state *game, uint8_t grid_x, uint8_t grid_y) {
+  uint8_t type;
+  if(!grid_x || !grid_y)
+    return;
+  
+  type = game->level[game->current_level].tiles[grid_y*100+grid_x];
+
+  switch(type) {
+    // add score and special item cases here later
+    default: break;
+  }
+  
+  game->level[game->current_level].tiles[grid_y*100+grid_x] = 0;
+
+  game->check_pickup_x = 0;
+  game->check_pickup_y = 0;
+}
+
+void draw_world(struct game_state *game, struct game_assets *assets, SDL_Renderer *renderer){
   uint8_t i,j;
   SDL_Rect dest;
   uint8_t tile_index;
 
   for (j = 0; j < 10; j++)
   {
-    dest.y = j*16;
-    dest.w = 16;
-    dest.h = 16;
+    dest.y = j*TILE_SIZE;
+    dest.w = TILE_SIZE;
+    dest.h = TILE_SIZE;
     for (i = 0; i < 20; i++)
     {
-      dest.x = i * 16;
+      dest.x = i * TILE_SIZE;
       tile_index = game->level[game->current_level].tiles[j*100+game->view_x+i];
       SDL_RenderCopy(renderer, assets->graphics_tiles[tile_index], NULL, &dest);
     }
-    SDL_RenderPresent(renderer);
+
   }
-  
+}
+
+void draw_dave(struct game_state *game, struct game_assets *assets, SDL_Renderer *renderer){
+  SDL_Rect dest;
+  dest.x = game->dave_px;
+  dest.y = game->dave_py;
+  dest.w = 20;
+  dest.h = 16;
+
+  SDL_RenderCopy(renderer, assets->graphics_tiles[56], NULL, &dest);
+}
+
+uint8_t is_clear(struct game_state *game, uint16_t px, uint16_t py){
+  uint8_t grid_x;
+  uint8_t grid_y;
+  uint8_t type;
+
+  grid_x = px / TILE_SIZE;
+  grid_y = py / TILE_SIZE;
+  type = game->level[game->current_level].tiles[grid_y*100+grid_x];
+
+  if(type == 1) {return 0;}
+  if(type == 3) {return 0;}
+  if(type == 5) {return 0;}
+  if(type == 15) {return 0;}
+  if(type == 16) {return 0;}
+  if(type == 17) {return 0;}
+  if(type == 18) {return 0;}
+  if(type == 19) {return 0;}
+  if(type == 21) {return 0;}
+  if(type == 22) {return 0;}
+  if(type == 23) {return 0;}
+  if(type == 24) {return 0;}
+  if(type == 29) {return 0;}
+  if(type == 30) {return 0;}
+
+  switch(type) {
+    case 10:
+    case 47:
+    case 48:
+    case 49:
+    case 50:
+    case 51:
+    case 52:
+    {
+      game->check_pickup_x = grid_x;
+      game->check_pickup_y = grid_y;
+    } 
+      break;
+    default: 
+    break;
+  }
+
+  return 1;
 }
--- lmdave.h
+++ lmdave.h
@@ -9,11 +9,33 @@ struct dave_level{
 };
 
 struct game_state{
+  double_t time_scale;
+  
   uint8_t quit;
   uint8_t current_level;
   uint8_t view_x;
   uint8_t view_y;
   int8_t scroll_x;
+  uint8_t dave_x;
+  uint8_t dave_y;
+  uint16_t dave_px;
+  uint16_t dave_py;
+
+  uint8_t try_right;
+  uint8_t try_left;
+  uint8_t try_jump;
+
+  uint8_t dave_right;
+  uint8_t dave_left;
+  uint8_t dave_jump;
+  uint8_t jump_timer;
+  uint8_t check_pickup_x;
+  uint8_t check_pickup_y;
+
+  uint8_t on_ground;
+
+  uint8_t collision_point[9];
+
 
   struct dave_level level[10];
 };
@@ -27,4 +49,15 @@ void init_game(struct game_state *);
 void init_assets(struct game_assets *, SDL_Renderer *);
 void check_input(struct game_state *);
 void update_game(struct game_state *);
+void check_collision(struct game_state *);
+void pickup_item(struct game_state *, uint8_t, uint8_t);
 void render(struct game_state *, SDL_Renderer *, struct game_assets *);
+void verify_input(struct game_state *game);
+void scroll_screen(struct game_state *);
+void move_dave(struct game_state *);
+void apply_gravity(struct game_state *);
+void clear_input(struct game_state *);
+void draw_world(struct game_state *, struct game_assets *, SDL_Renderer *);
+void draw_dave(struct game_state *, struct game_assets *, SDL_Renderer *);
+
+uint8_t is_clear(struct game_state *, uint16_t, uint16_t);
